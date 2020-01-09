127d68744b7d810469035239c0ffa839b2ded8b0
Kwame Bonsu <kwamebonsu92@gmail.com>
2020-01-09 14:59:16 +0000

    users table seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = new User;
-        $user->name = 'Jemimah';
-        $user->email = 'jemimah@gmail.com';
-        $user->mobile = '0248562578';
-        $user->password = '123456';
-        $user->address = 'Kotei Subway Surfers';
-        $user->bday = '1993-02-01';
-        $user->save();
+        factory(App\User::class, 50)->create();
     }
 }
